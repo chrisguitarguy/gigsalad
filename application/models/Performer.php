8e635e42b86a89afe6e33f10e3898fe4bbc0086f
Christopher Davis <cdavis9999@gmail.com>
2016-04-02 09:06:21 -0400

    Run Performers `thumbnail` Field Through `json_decode`

--- application/models/Performer.php
+++ application/models/Performer.php
@@ -6,12 +6,21 @@ final class Performer extends CI_Model
     public function findAll()
     {
         $q = $this->db->get('performers');
-        return $q->result();
+        return array_map([$this, 'normalizeRow'], $q->result());
     }
 
     public function getByIdentifier($id)
     {
         $q = $this->db->get_where('performers', ['ID' => $id], 1);
-        return $q->num_rows() > 0 ? $q->row() : null;
+        return $q->num_rows() > 0 ? $this->normalizeRow($q->row()) : null;
+    }
+
+    private function normalizeRow($row)
+    {
+        if (!empty($row->thumbnail)) {
+            $row->thumbnail = json_decode($row->thumbnail);
+        }
+
+        return $row;
     }
 }
